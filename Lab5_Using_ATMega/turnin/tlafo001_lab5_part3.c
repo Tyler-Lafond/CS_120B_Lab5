9ad2eb3acdffb5ccebc23296ea4fc5aa05424d28
Tyler-Lafond <39169194+Tyler-Lafond@users.noreply.github.com>
2021-10-16 23:45:44 -0700

    Add files via upload

--- Lab5_Using_ATMega/turnin/tlafo001_lab5_part3.c
+++ Lab5_Using_ATMega/turnin/tlafo001_lab5_part3.c
@@ -6,6 +6,8 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *      Demo: https://youtu.be/Ko0cSCSKu-k 
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
